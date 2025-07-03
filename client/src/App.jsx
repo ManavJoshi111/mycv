a0b6ae5f6fbb4c4b01457ec9c33704f6807133cf
ManavJoshi <manavjoshi154@gmail.com>
2025-07-03 21:55:39 +0530

    CHORE: Add prettier; manage workspaces

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-import "./Styles/main.css";
-import Navbar from "./Components/Navbar";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Styles/main.css';
+import Navbar from './Components/Navbar';
 
 const App = () => {
   return (
@@ -14,7 +11,7 @@ const App = () => {
       <Router>
         <Navbar />
       </Router>
-      <ToastContainer style={{ width: "400px" }} />
+      <ToastContainer style={{ width: '400px' }} />
     </>
   );
 };
