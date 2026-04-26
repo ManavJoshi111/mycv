ddd0f962f917460f3709d04c8ad04c2333ef5d3a
ManavJoshi <manavjoshi154@gmail.com>
2026-04-26 19:13:43 +0530

    start from scratch, with mycv v2

--- README.md
+++
@@ -1,3 +0,0 @@
-# Resume Builder
-
-A project that allows users to create and manage their resumes easily.
--- client/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-import { createAuthStore } from './authStore';
-
-const useStore = create((...a) => ({
-  ...createAuthStore(...a),
-}));
-
-export default useStore;
